f74cd9d5da91c5fe68e7de036a0d553b02b86d24
viet2457 <92298274+viet2457@users.noreply.github.com>
2022-04-19 16:30:43 +0700

    Add files via upload

---
+++ ChatPanel.java
@@ -0,0 +1,144 @@
+package ck2;
+
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.border.TitledBorder;
+import java.awt.GridLayout;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Font;
+
+@SuppressWarnings("serial")
+public class ChatPanel extends JPanel {
+	
+	Socket socket = null;
+	BufferedReader bf = null;
+	DataOutputStream os = null;
+	OutputThread ot = null;
+	String sender;
+	String receiver;
+	JTextArea txtMessages;
+	private static String DB_URL = "jdbc:mysql://localhost:3306/test";
+	private static String USER_NAME = "root";
+	private static String PASSWORD = "";
+	/**
+	 * Create the panel.
+	 */
+	public ChatPanel(Socket s, String sender, String receiver) {
+		 try {
+	            
+	            Connection conn = getConnection(DB_URL, USER_NAME, PASSWORD);
+	            
+	            Statement stmt = conn.createStatement();
+	          
+	            ResultSet rs = stmt.executeQuery("select * from informationchat");
+	           
+	            while (rs.next()) {
+	                System.out.println(rs.getString(1));
+	            }
+	            
+	            conn.close();
+	       
+	    
+		setLayout(new BorderLayout(0, 0));
+		
+		JPanel panel = new JPanel();
+		panel.setBorder(new TitledBorder(null, "Message", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		add(panel, BorderLayout.SOUTH);
+		panel.setLayout(new GridLayout(0, 3, 0, 0));
+		
+		JScrollPane scrollPane = new JScrollPane();
+		panel.add(scrollPane);
+		
+		JTextArea txtMessage = new JTextArea();
+		scrollPane.setViewportView(txtMessage);
+		
+		JButton btnSend = new JButton("Send");
+		btnSend.setFont(new Font("Tahoma", Font.BOLD, 16));
+		btnSend.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (txtMessage.getText().trim().length()==0) return;
+				try {
+					os.writeBytes(txtMessage.getText());
+					os.write(13); os.write(10);
+					os.flush();
+					txtMessages.append("\n"+sender+ ":"+ txtMessage.getText());
+					txtMessage.setText("");
+				}
+				catch (Exception ex) {
+					ex.printStackTrace();
+				}
+			}
+		});
+		panel.add(btnSend);
+		
+		JButton btnNewButton = new JButton("Save");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+					System.out.print("insert into informationchat values (N\'"+txtMessage.getText()+"\'+)");
+					stmt.executeUpdate("insert into informationchat values (N\'"+txtMessages.getText()+"\'+)");
+				} catch (Exception e2) {
+					// TODO: handle exception
+					e2.printStackTrace();
+					System.out.print(e2);
+				}
+			}
+		});
+		btnNewButton.setFont(new Font("Dialog", Font.BOLD, 16));
+		panel.add(btnNewButton);
+		
+		JScrollPane scrollPane_1 = new JScrollPane();
+		add(scrollPane_1, BorderLayout.CENTER);
+		
+		txtMessages = new JTextArea();
+		scrollPane_1.setViewportView(txtMessages);
+
+		socket = s;
+		this.sender = sender;
+		this.receiver = receiver;
+		try {
+			bf = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			os = new DataOutputStream(socket.getOutputStream());
+			ot = new OutputThread(s, txtMessages, sender, receiver);
+			ot.start();
+		} 
+		catch (Exception e) {
+			
+		}
+	
+	} catch (Exception ex) {
+		ex.printStackTrace();
+	}
+}
+	private Connection getConnection(String dbURL, String userName, String password) {
+		// TODO Auto-generated method stub
+		 Connection conn = null;
+	        try {
+	            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection(dbURL, userName, password);
+	            System.out.println("connect successfully!");
+	        } catch (Exception ex) {
+	            System.out.println("connect failure!");
+	            ex.printStackTrace();
+        }
+	        
+	        return conn;
+    }
+	public JTextArea gettxtMessages() {
+		return this.txtMessages;
+	}
+
+}
---
+++ ClientChatter.java
@@ -0,0 +1,132 @@
+package ck2;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.TitledBorder;
+
+import ck2.ChatPanel;
+
+import java.awt.GridLayout;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.SwingConstants;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class ClientChatter extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField txtStaff;
+	private JTextField txtServerIP;
+	private JTextField txtServerPort;
+
+	Socket mngSocket = null;
+	String mngIP = "";
+	int mngPort = 0;
+	String staffName = "";
+	BufferedReader bf = null;
+	DataOutputStream os = null;
+	OutputThread ot = null;
+	
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ClientChatter frame = new ClientChatter();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public ClientChatter() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 629, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		JPanel panel = new JPanel();
+		panel.setBorder(new TitledBorder(null, "Staff and Server info.", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		contentPane.add(panel, BorderLayout.NORTH);
+		panel.setLayout(new GridLayout(1, 7, 0, 0));
+		
+		JLabel lblNewLabel = new JLabel("Staff:  ");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.RIGHT);
+		panel.add(lblNewLabel);
+		
+		txtStaff = new JTextField();
+		panel.add(txtStaff);
+		txtStaff.setColumns(10);
+		
+		JLabel lblNewLabel_1 = new JLabel("Mng IP:  ");
+		lblNewLabel_1.setHorizontalAlignment(SwingConstants.RIGHT);
+		panel.add(lblNewLabel_1);
+		
+		txtServerIP = new JTextField();
+		txtServerIP.setText("localhost");
+		panel.add(txtServerIP);
+		txtServerIP.setColumns(10);
+		
+		JLabel lblNewLabel_2 = new JLabel("Port:  ");
+		lblNewLabel_2.setHorizontalAlignment(SwingConstants.RIGHT);
+		panel.add(lblNewLabel_2);
+		
+		txtServerPort = new JTextField();
+		txtServerPort.setText("12340");
+		panel.add(txtServerPort);
+		txtServerPort.setColumns(10);
+		
+		JFrame thisFrame = this;
+		
+		JButton btnConnect = new JButton("Connect");
+		btnConnect.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				mngIP = txtServerIP.getText();
+				mngPort = Integer.parseInt(txtServerPort.getText());
+				staffName = txtStaff.getText();
+				try {
+					mngSocket = new Socket(mngIP,mngPort);
+				if(mngSocket != null) {
+					ChatPanel p = new ChatPanel(mngSocket, staffName, "Manager");
+					thisFrame.getContentPane().add(p);
+					p.gettxtMessages().append("Manager is running");
+					p.updateUI();
+					
+					bf = new BufferedReader(new InputStreamReader(mngSocket.getInputStream()));
+					os= new DataOutputStream(mngSocket.getOutputStream());
+					
+					os.writeBytes("Staff:"+staffName);
+					os.write(13);
+					os.write(10);
+					os.flush();
+					
+					}
+				} 
+				catch (Exception ex) {
+					ex.printStackTrace();
+				}
+			}
+		});
+		panel.add(btnConnect);
+	}
+}
